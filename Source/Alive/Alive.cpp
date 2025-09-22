ad81677e900075d5b23014c666f5680065995eb0
Stimpicus <56415687+Stimpicus@users.noreply.github.com>
2025-09-22 17:04:30 -0400

    Initial commit

---
+++ Source/Alive/Alive.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Alive.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Alive, "Alive" );
